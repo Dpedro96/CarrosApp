9ae567d6b54cd32201fb64ad762c4404463e0f3a
Dpedro96 <pedrohferreira300402@gmail.com>
2023-12-10 18:31:33 -0300

    signin and signup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./view/home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./view/carros/home/home.module').then( m => m.HomePageModule)
   },
   {
     path: '',
@@ -13,12 +13,21 @@ const routes: Routes = [
   },
   {
     path: 'detalhes',
-    loadChildren: () => import('./view/detalhes/detalhes.module').then( m => m.DetalhesPageModule)
+    loadChildren: () => import('./view/carros/detalhes/detalhes.module').then( m => m.DetalhesPageModule)
   },
   {
     path: 'cadastro',
-    loadChildren: () => import('./view/cadastro/cadastro.module').then( m => m.CadastroPageModule)
+    loadChildren: () => import('./view/carros/cadastro/cadastro.module').then( m => m.CadastroPageModule)
   },
+  {
+    path: 'signin',
+    loadChildren: () => import('./view/usuario/signin/signin.module').then( m => m.SigninPageModule)
+  },  {
+    path: 'signup',
+    loadChildren: () => import('./view/usuario/signup/signup.module').then( m => m.SignupPageModule)
+  },
+
+
 ];
 
 @NgModule({
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebaseConfig : {
- apiKey: "AIzaSyDFlwhHr12V6Tg9apW2R5RUjltAqoz8sDE",
-  authDomain: "carros-7bfad.firebaseapp.com",
-  projectId: "carros-7bfad",
-  storageBucket: "carros-7bfad.appspot.com",
-  messagingSenderId: "837727451005",
-  appId: "1:837727451005:web:c6bd5e7551050a08947e14",
-  measurementId: "G-58WVF5GDWW"
+    apiKey: "AIzaSyCv-7eeVDh5oAhIDHKjWLe_giKSrB_ig6o",
+    authDomain: "app-carros-4eb77.firebaseapp.com",
+    projectId: "app-carros-4eb77",
+    storageBucket: "app-carros-4eb77.appspot.com",
+    messagingSenderId: "630037306472",
+    appId: "1:630037306472:web:eff5bd817ae68fc056efdd",
+    measurementId: "G-MSLJR0Y6NY"
   }
 };
